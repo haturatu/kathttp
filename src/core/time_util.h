b59c466133804023ff5839600f0b9d633c7fcb93
haturatu <taro@eyes4you.org>
2026-07-13 01:14:39 +0900

    fix(ci): make native quality checks buildable

--- src/core/time_util.h
+++ src/core/time_util.h
@@ -8,7 +8,7 @@ namespace kathttp {
 
 /* Nanosecond timestamp used throughout ngtcp2 / nghttp3. */
 inline uint64_t timestamp_now_ns() {
-    struct timespec ts{};
+    timespec ts{};
     clock_gettime(CLOCK_MONOTONIC, &ts);
     return static_cast<uint64_t>(ts.tv_sec) * 1000000000ull + ts.tv_nsec;
 }
